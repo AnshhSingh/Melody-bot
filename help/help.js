c785995a76367e9a13239b28ebae6f8a91459082
Ansh <ANSHSINGH25BD@GMAIL.COM>
2021-07-05 09:50:37 +0530

    Update and rename helpr.js to help.js

--- help/help.js
+++ help/help.js
@@ -6,13 +6,12 @@ module.exports = {
         const help = new Discord.MessageEmbed()
         .setColor('#00faf0')
         .setTitle('Help')
-        //.setDescription('[Our support server](https://discord.com/invite/RKTh6fC)• | •[Invite this bot](https://discord.com/oauth2/authorize?client_id=636932023717593088&scope=bot%20applications.commands&permissions=1312685425)•')
-        .addFields(
+        
             { name: ';musichelp', value: 'for all music commands', inline: true },
             { name: ';funhelp', value: 'fun and interesting commands such as kiss,hug,poll and some image related command', inline: true },
             { name: ';imghelp', value: 'all image related commands', inline: true },
             { name: ';nsfwhelp', value: 'all nice nsfw commands XD', inline: true },
-            { name: ';mchelp', value: 'minecraft related commands', inline: true },
+            //{ name: ';mchelp', value: 'minecraft related commands', inline: true },
             { name: ';utihelp', value: 'utility search and server related commands', inline: true },
         )
         .setFooter(``)
